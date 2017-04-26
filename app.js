98ef9017d11c6c0810bdfc3abe0223de6e9155e0
DONGHYUN KIM <kdh2007@gmail.com>
2017-04-26 13:19:34 -0400

    Update app.js
    change

--- app.js
+++ app.js
@@ -13,11 +13,11 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
   
 // Create chat bot
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    //appId: process.env.MICROSOFT_APP_ID,
+    //appPassword: process.env.MICROSOFT_APP_PASSWORD
 
-//    appId: '27ea08b6-c5c9-4c2b-b9cb-ee7e1534eb06',
- //   appPassword: 'KBifN4S7usjS4qHx2eWYWK2'
+    appId: '27ea08b6-c5c9-4c2b-b9cb-ee7e1534eb06',
+    appPassword: 'KBifN4S7usjS4qHx2eWYWK2'
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
